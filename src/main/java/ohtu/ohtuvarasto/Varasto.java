e40c8eb2a678e0b0747710ab93e8fa8bde75967c
Niclas Forsman <niclas.forsman@futurice.com>
2019-11-04 21:01:40 +0200

    Revert "break code climate"
    This reverts commit 0953dcb82fb5250877caf01505e304b06d4c06b8.

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -15,7 +15,8 @@ public class Varasto {
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
         setTilavuus(tilavuus);
 
-        if (alkuSaldo < 0.0) {
+        if (alkuSaldo < 0.0)
+        {
             this.saldo = 0.0;
         } else if (alkuSaldo <= tilavuus) // mahtuu
         {
